1b68656d078923765d62a66225d60166be5b1240
Dandelion <51200196+Dandelionss@users.noreply.github.com>
2019-09-05 21:42:18 +0800

    Create pixiv.js

---
+++ pixiv.js
@@ -0,0 +1,14 @@
+addEventListener("fetch", event => {
+  let url = new URL(event.request.url);
+  url.hostname = "i.pximg.net";
+
+  let request = new Request(url, event.request);
+  event.respondWith(
+    fetch(request, {
+      headers: {
+        'Referer': 'https://www.pixiv.net/',
+        'User-Agent': 'Cloudflare Workers'
+      }
+    })
+  );
+});
